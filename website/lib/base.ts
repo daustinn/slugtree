88ac172e3226d2fbb77e1583e0705c845c3ecf92
Daustinn <daustinn.music@gmail.com>
2026-08-22 17:54:43 -0500

    Astro website initialized

---
+++ website/lib/base.ts
@@ -0,0 +1,18 @@
+export default (to: string) => {
+  let base = ''
+  try {
+    const { pathname } = new URL(
+      import.meta.env.SITE + (import.meta.env.SITE_BASE ?? '')
+    )
+
+    base = pathname.endsWith('/') ? pathname.slice(0, -1) : pathname
+  } catch {
+    base = ''
+  }
+
+  const cleanTo = to?.startsWith('/') ? to : `/${to}`
+
+  if (base && cleanTo === '/') return base
+
+  return `${base}${cleanTo}`
+}
