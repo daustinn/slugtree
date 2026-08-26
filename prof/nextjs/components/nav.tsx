ee29b091fdfb8bbf6f090880692331334c911cd2
Daustinn <daustinn.music@gmail.com>
2026-08-25 22:01:44 -0500

    fix(prof): update navigation and eslint configurations in prof workspaces

--- prof/nextjs/components/nav.tsx
+++ prof/nextjs/components/nav.tsx
@@ -1,12 +1,13 @@
+import Link from 'next/link'
 import { NextJs, Slugtree, SlugtreeIcon } from './icons'
 
 export default function Nav() {
   return (
     <nav className="h-15 min-h-15 container flex items-center gap-5">
-      <a href="/slugtree" className="flex items-center">
+      <Link href="/slugtree" className="flex items-center">
         <SlugtreeIcon width={25} />
         <Slugtree width={80} />
-      </a>
+      </Link>
       <span className="inline-flex h-5 rotate-14 w-px bg-foreground/30"></span>
       <a
         className="font-medium text-lg flex items-center gap-2"
